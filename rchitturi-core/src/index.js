560b2b8d90da3009a4f06ddec6f9bc6ea5a64da8
Rahul2309 <rahulchitturi420@gmail.com>
2020-12-06 23:45:40 +0530

    'sitemap'

--- rchitturi-core/src/index.js
+++ rchitturi-core/src/index.js
@@ -3,21 +3,27 @@ import {hydrate,render} from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
+import { BrowserRouter } from "react-router-dom";
 import { icons } from "./assets/icons";
 
 import { Provider } from "react-redux";
 import store from "./stock";
 
- React.icons = icons;
-
+React.icons = icons;
  const rootElement = document.getElementById('root');
  if (rootElement.hasChildNodes()) {
-     hydrate(<Provider store={store}>
+     hydrate(
+      <BrowserRouter>
+      <Provider store={store}>
       <App />
-    </Provider>, rootElement);
+    </Provider>
+    </BrowserRouter>, rootElement);
  } else {
-     render(<Provider store={store}>
+     render(
+      <BrowserRouter>
+     <Provider store={store}>
       <App />
-    </Provider>, rootElement);
+    </Provider>
+    </BrowserRouter>, rootElement);
  }
 serviceWorker.unregister();
