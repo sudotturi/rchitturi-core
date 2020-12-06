dcd7c2531fab7856cb2f4c43be0ad06b8fe8ea91
Rahul2309 <rahulchitturi420@gmail.com>
2020-12-06 15:43:33 +0530

    'sitemap'

---
+++ rchitturi-core/src/sitemap-routes.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Route } from 'react-router';
+export default (
+    
+    <Route>
+	<Route path='/' />
+	<Route path='/dashboard' />
+	<Route path='/dashboard/covid19' />
+    </Route>
+);
--- rchitturi-core/src/utils/routes.js
+++ rchitturi-core/src/utils/routes.js
@@ -2,11 +2,10 @@ import React from 'react';
 
 // /const Dashboard = React.lazy(() => import('../views/dashboard/Dashboard'));
 const Covid19 = React.lazy(() => import('../views/dashboard/covid/Covid19'));
-
 const Home= React.lazy(() => import('../views/home/Home'));
 
 const routes = [
-  { path: "/", exact: true, name: "Home", component:Home },
+  { path: '/', exact: true, name: "Home", component:Home },
   { path: '/dashboard', name: 'Dashboard', component: Covid19, exact: true },
   { path: '/dashboard/covid19', name: 'Covid 19', component: Covid19 }
   
