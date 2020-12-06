0a9c17a1a302ace6317c0221b93582a487795202
Rahul2309 <rahulchitturi420@gmail.com>
2020-12-06 20:44:37 +0530

    'sitemap'

--- rchitturi-core/src/sitemap-generator.js
+++ rchitturi-core/src/sitemap-generator.js
@@ -8,7 +8,7 @@ require("babel-register")({
   function generateSitemap() {
       return (
         new Sitemap(router)
-            .build("https://www.chitturi.ml/#/")
+            .build("https://www.chitturi.ml/")
             .save("./public/sitemap.xml")
       );
   }
